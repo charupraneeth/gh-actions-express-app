e72bb5e93c16137693c8d4d3123eb06c8dbe71aa
charupraneeth <charupraneeth@gmail.com>
2023-01-19 17:20:15 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
-# pull the latest bull-app images from GCR
-sudo docker compose -f docker-compose.dev.yml pull		
-sudo docker compose -f docker-compose.dev.yml down 
-sudo docker compose -f docker-compose.dev.yml up
+#!/bin/bash
+
 # Remove the older images marked an 'none'
 sudo docker image rm $(sudo docker images -f 'dangling=true' -q)
+# pull the latest bull-app images from GCR
+sudo docker compose -f docker-compose.yml pull		
+sudo docker compose -f docker-compose.yml down 
+sudo docker compose -f docker-compose.yml up
