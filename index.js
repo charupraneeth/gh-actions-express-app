e3f8381044b4c82c91c4d91db6bbf142277bdf9a
charupraneeth <charupraneeth@gmail.com>
2023-01-19 11:40:13 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+/* eslint-disable */
+const port = process.env.PORT || 1337;
+/* eslint-enable */
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "hello 👋 from the index route ",
+  });
+});
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  next(error);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV == "development" ? err.stack : "🍔",
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`app listening at port : ${port}`));
