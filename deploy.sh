e084d9ff3bbb51a3583edf0065f95a47059cc7c9
charupraneeth <charupraneeth@gmail.com>
2023-01-20 11:45:30 +0530

    [mark-v1.3.2]

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ sudo docker image rm $(sudo docker images -f 'dangling=true' -q)
 # pull the latest bull-app images from GCR
 sudo docker compose -f docker-compose.yml pull		
 sudo docker compose -f docker-compose.yml down 
-sudo docker compose -f docker-compose.yml up
+sudo docker compose -f docker-compose.yml up -d
